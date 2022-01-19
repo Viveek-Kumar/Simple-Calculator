aaba866f1a2d9c70f151829a3cccbf228450f5f1
vivek <vk580182@gmail.com>
2022-01-19 17:19:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const screen = document.getElementById('screen');
+buttons  = document.querySelectorAll('button');
+let screenValue = '';
+// print data diplayed on button
+for(item of buttons){
+    item.addEventListener('click',(e) =>{
+        buttonText = e.target.innerText;
+        console.log('Button text is',buttonText);
+        if(buttonText == 'X'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }else if(buttonText == 'AC'){
+            screenValue = "";
+            screen.value = screenValue;
+        }else if(buttonText == '='){
+            screen.value = eval(screenValue);
+        }else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
